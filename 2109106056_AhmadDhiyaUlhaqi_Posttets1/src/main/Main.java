615c9c97d1c60e6d2696273310dd6ceaa1c73ee7
DH1YA <89460039+DH1YA@users.noreply.github.com>
2023-03-07 21:18:46 +0800

    Update Main.java

--- 2109106056_AhmadDhiyaUlhaqi_Posttets1/src/main/Main.java
+++ 2109106056_AhmadDhiyaUlhaqi_Posttets1/src/main/Main.java
@@ -68,6 +68,9 @@ public class Main {
     
       switch (pilAdmin) {
           case 1 :
+              System.out.println("\n====================");
+              System.out.println("   Daftar Menu    ");
+              System.out.println("====================");
               lihatMenu();
               admin();
               break;
@@ -76,47 +79,11 @@ public class Main {
               admin();
               break;
           case 3 :
-              System.out.println("====================");
-              System.out.println("    Hapus Menu    ");
-              System.out.println("====================");
-              if(dtMenu.size() <= 0){
-                System.out.println("DAFTAR MENU MASIH KOSONG");             
-              }else{
-                lihatMenu();
-                System.out.println("Pilih nomor menu yang ingin dihapus : "); int noHapus = Integer.parseInt(input.readLine());
-                for (int i = 0; i < dtMenu.size(); i++) {
-                  if(i == (noHapus-1)){
-                      dtMenu.remove(i);
-                  }
-                  if(i >= dtMenu.size() && i != (noHapus-1)){
-                      System.out.println("Pilihan Tidak Tersedia");
-                  }
-                }                
-              }
-
+              hapusMenu();
               admin();
               break;
           case 4 :
-              System.out.println("====================");
-              System.out.println("    Edit Menu    ");
-              System.out.println("====================");
-              if(dtMenu.size() <= 0){
-                System.out.println("DAFTAR MENU MASIH KOSONG");
-              }else{
-              lihatMenu();
-              System.out.print("Pilih nomor menu yang ingin diubah : "); int noUbah = Integer.parseInt(input.readLine());
-              for (int i = 0; i < dtMenu.size(); i++) {
-                  if(i == (noUbah-1)){
-                      System.out.print("Nama Menu : "); dtMenu.get(i).namaMenu = input.readLine();
-                      System.out.print("Harga : ");dtMenu.get(i).hargaMenu = Integer.parseInt(input.readLine());
-                      System.out.print("Deskripsi");dtMenu.get(i).deskripsiMenu = input.readLine();
-                  }
-                  if(i >= dtMenu.size()){
-                      System.out.println("Pilihan Tidak Tersedia");
-                  }
-                }                  
-              }
-
+              editMenu();
               admin();
               break;
           case 5 :
@@ -130,9 +97,6 @@ public class Main {
   }
 
 public static void lihatMenu(){ 
-    System.out.println("\n====================");
-    System.out.println("   Daftar Menu    ");
-    System.out.println("====================");
     if(dtMenu.size() <= 0){
         System.out.println("DAFTAR MENU MASIH KOSONG");
     }else {
@@ -163,6 +127,50 @@ public static void tambahMenu() throws IOException{
     System.out.print("Masukkan Deskripsi Menu : "); String deskripsi = input.readLine();
     var menu = new Menu(namaMenu,deskripsi,harga);
     dtMenu.add(menu);
+}
+
+public static void hapusMenu() throws IOException{
+    System.out.println("====================");
+    System.out.println("    Hapus Menu    ");
+    System.out.println("====================");
+    if(dtMenu.size() <= 0){
+      System.out.println("DAFTAR MENU MASIH KOSONG");             
+    }else{
+      lihatMenu();
+      System.out.println("Pilih nomor menu yang ingin dihapus : "); int noHapus = Integer.parseInt(input.readLine());
+      for (int i = 0; i < dtMenu.size(); i++) {
+        if(i == (noHapus-1)){
+            dtMenu.remove(i);
+        }
+        if(noHapus > dtMenu.size()){
+            System.out.println("!!!Pilihan Tidak Tersedia!!!\n");
+            hapusMenu();
+        }
+      }                
+    }    
+}
+
+public static void editMenu() throws IOException{
+    System.out.println("====================");
+    System.out.println("    Edit Menu    ");
+    System.out.println("====================");
+    if(dtMenu.size() <= 0){
+      System.out.println("DAFTAR MENU MASIH KOSONG");
+    }else{
+    lihatMenu();
+    System.out.print("Pilih nomor menu yang ingin diubah : "); int noUbah = Integer.parseInt(input.readLine());
+    for (int i = 0; i < dtMenu.size(); i++) {
+        if(i == (noUbah-1)){
+            System.out.print("Nama Menu : "); dtMenu.get(i).namaMenu = input.readLine();
+            System.out.print("Harga : ");dtMenu.get(i).hargaMenu = Integer.parseInt(input.readLine());
+            System.out.print("Deskripsi : ");dtMenu.get(i).deskripsiMenu = input.readLine();
+        }
+        if(noUbah > dtMenu.size()){
+            System.out.println("!!!Pilihan Tidak Tersedia!!!\n");
+            editMenu();
+        }
+      }                  
+    }  
 }
 
 }
